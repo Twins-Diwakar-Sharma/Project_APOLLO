40e4c589eefd908a35e64b7d6c0f620368a24471
Twins-Divyanshu-Sharma <twins-divyanshu-sharma@github.com>
2022-07-25 21:18:25 +0530

    Changed .gitignore to ignore binaries

--- C++/source/Activation.cpp
+++ C++/source/Activation.cpp
@@ -3,11 +3,16 @@
 Activation::Activation(float(*func)(float), float(*diff)(float)) : fx(func), dfx(diff)
 {}
 
-Activation act::sigmoid = Activation( [](float f)->float{
+Activation act::sigmoid( 
+        [](float f)->float
+        {
              return (1.0f/(1.0f + exp(-f))); 
-        }, [](float f)->float{
+        }, 
+        [](float f)->float
+        {
             return f * (1.0f - f);
-        });
+        }
+);
 
 
 
